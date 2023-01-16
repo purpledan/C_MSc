0f0b96a9cdbafdc52dd8db2bd7f585e51b02cee8
Daniel <s212281283@mandela.ac.za>
2023-01-16 20:04:26 +0200

    Updated MsgQ

---
+++ TST_MsgQueue.c
@@ -0,0 +1,5 @@
+//
+// Created by Daniel on 2023/01/16.
+//
+
+#include "TST_MsgQueue.h"
---
+++ TST_MsgQueue.h
@@ -0,0 +1,8 @@
+//
+// Created by Daniel on 2023/01/16.
+//
+
+#ifndef C_MSC_TST_MSGQUEUE_H
+#define C_MSC_TST_MSGQUEUE_H
+
+#endif //C_MSC_TST_MSGQUEUE_H
