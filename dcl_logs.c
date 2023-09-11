f50e798a93247e909e48b551f889bea3d06de7ff
Purpledan <s212281283@mandela.ac.za>
2023-09-11 13:46:53 +0200

    Attempt to revert merging, help!

--- dcl_logs.c
+++
@@ -1,7 +0,0 @@
-/*******************************************************************************
- * Copyright (c) 2023. Daniel Janse van Rensburg - All Rights Reserved
- * Dan's Control Library (DCL) is subject to future license changes.
- ******************************************************************************/
-/* Created by dan on 2023/07/06. */
-
-#include "dcl_logs.h"
